94465663bc5d8b94f94b9ccf876a72075cefd30c
Adrian Darian <adarian@ucmerced.edu>
2019-03-13 16:21:15 -0700

    complete todo

--- main.js
+++ main.js
@@ -1,56 +1,69 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+'use strict'
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+const path = require('path')
+const { app, ipcMain } = require('electron')
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
+const Window = require('./Window')
+const DataStore = require('./DataStore')
+
+require('electron-reload')(__dirname)
+
+// create a new todo store name "Todos Main"
+const todosData = new DataStore({ name: 'Todos Main' })
+
+function main () {
+  // todo list window
+  let mainWindow = new Window({
+    file: path.join('renderer', 'index.html')
+  })
+
+  // add todo window
+  let addTodoWin
+
+  // TODO: put these events into their own file
+
+  // initialize with todos
+  mainWindow.once('show', () => {
+    mainWindow.webContents.send('todos', todosData.todos)
+  })
+
+  // create add todo window
+  ipcMain.on('add-todo-window', () => {
+    // if addTodoWin does not already exist
+    if (!addTodoWin) {
+      // create a new add todo window
+      addTodoWin = new Window({
+        file: path.join('renderer', 'add.html'),
+        width: 400,
+        height: 400,
+        // close with the main window
+        parent: mainWindow
+      })
+
+      // cleanup
+      addTodoWin.on('closed', () => {
+        addTodoWin = null
+      })
     }
   })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  // add-todo from add todo window
+  ipcMain.on('add-todo', (event, todo) => {
+    const updatedTodos = todosData.addTodo(todo).todos
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+    mainWindow.send('todos', updatedTodos)
+  })
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
+  // delete-todo from todo list window
+  ipcMain.on('delete-todo', (event, todo) => {
+    const updatedTodos = todosData.deleteTodo(todo).todos
+
+    mainWindow.send('todos', updatedTodos)
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', main)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  app.quit()
 })
-
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
